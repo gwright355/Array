3e5ab6759786baa6108b8baaafac097e03a1d2f7
Glenn <Glenn@DESKTOP-76J73F3>
2019-01-27 18:25:08 -0500

    needed

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Glenn Wright
+// Array assignment
+// ISM 4300
+
+
+
+using System;
 
 namespace Array
 {
@@ -6,7 +12,26 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] elements = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+            foreach (int i in elements)
+
+            {
+
+                Console.WriteLine("Element Value = " + i);
+
+            }
+
+            Console.WriteLine("__________________________________");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Press any key to exit the program.");
+            Console.ReadKey(true);
+
+            
+
+
         }
     }
 }
